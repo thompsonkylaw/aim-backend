3189560744a2f8c0d311902af80faa8c63b40f8e
thompsonkylaw <71827535+thompsonkylaw@users.noreply.github.com>
2025-03-08 11:58:56 +0800

    change console.log

--- index.js
+++ index.js
@@ -2,4 +2,9 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => res.send('Hello World!'));
-app.listen(process.env.PORT || 3000, () => console.log('Server running'));
+//app.listen(process.env.PORT || 3000, () => console.log('Server running'));
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
