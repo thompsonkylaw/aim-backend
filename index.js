53cb88c23124d68c184d740f09e81487cf8728ab
thompsonkylaw <71827535+thompsonkylaw@users.noreply.github.com>
2025-03-08 11:51:29 +0800

    add process.env.PORT

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => res.send('Hello World!'));
-app.listen(3000, () => console.log('Server running on port 3000'));
+app.listen(process.env.PORT || 3000, () => console.log('Server running'));
